8deea3ca1259a550d29be748ebcdd43397e2f4a3
Claudia Laurie <claudialaurie@college.harvard.edu>
2017-02-22 02:29:35 +0000

    graph header

---
+++ graph.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#define RANDOM 0
+#define EUCLIDIAN 1
+
+//edge structure declaration and initialization 
+typedef struct edge {
+    int start, end;
+    double weight;
+} edge;
+
+//vertex structure declaration and initialization 
+typedef struct vertex {
+    double* coordinates;
+} vertex;
+
+//graph structure declaration and initialization 
+typedef struct graph {
+    int num_v, num_e;
+    edge* edges;
+    vertex* vertices;
+} graph;
+
